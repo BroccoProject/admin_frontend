5cce66a0198b8a24aae739970092589835156924
Barry4747 <132226049+Barry4747@users.noreply.github.com>
2026-06-02 12:45:18 +0000

    Deploying to gh-pages from @ BroccoProject/admin_frontend@6cdb9f285dabd20e861bddc3c5c2b535889a9d8e 🚀

---
+++ chunk-7SHYTWCR.js
@@ -0,0 +1,1 @@
+var o={production:!0,apiUrl:"https://your-api-domain.com/api/v1"};export{o as a};
---
+++ chunk-WBWNHVFM.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-CSLXDHRP.js";import"./chunk-AWPMSNNZ.js";import"./chunk-7SHYTWCR.js";import"./chunk-OEBGCNEV.js";import"./chunk-CSWX6WDW.js";export{a as AuthPendingPage};
